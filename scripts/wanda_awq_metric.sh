b04769d24197f806079ba1a7e07c1c28be3042a1
Omar Yahia <oyahia@uci.edu>
2024-12-19 20:53:15 -0800

    updated code so far for different pruning Wanda metrics

--- scripts/wanda_awq_metric.sh
+++ scripts/wanda_awq_metric.sh
@@ -17,7 +17,8 @@ run_wanda () {
     --save $4 \
     --save_model $5 \
     --eval_seqlen $6 \
-    --awq_scales "/pub/oyahia/out/scales" \
+    --awq_scales "out/scales" \
+    --normalize \
     > $7
 }
 
@@ -38,21 +39,11 @@ run_quantize () {
         --w_bit $2 \
         --q_group_size 128 \
         --load_awq $3 \
-        --dump_quant $4 \
-        --q_backend $5
-}
-
-test_quantize() {
-    python -m awq.entry \
-        --model_path $1 \
-        --w_bit $2 \
-        --load_quant $3 \
-        --cache_dir "llm_weights" \
-        --q_group_size 128 \
-        --q_backend $4 \
+        --dump_fake $4 \
+        --q_backend $5 \
         --tasks "wikitext" \
-        --eval_seqlen $5 \
-    > $6
+        --eval_seqlen $6 \
+        > $7
 }
 
 wanda_awq() {
@@ -64,39 +55,35 @@ wanda_awq() {
 
     # llama-7b with wanda pruning method
     echo "Running with wanda pruning method"
-    # run_wanda $1 $2 $3 $4 $4 $5 $6
+    run_wanda $1 $2 $3 $4 $4 $5 $6
     echo "Finished wanda pruning method"
     awq_pipeline $4 $7 $8 $9 ${10} ${11} ${12}
 }
 
 awq_pipeline() {
-    # # AWQ
-    # echo "Running AWQ"
-    # run_awq $1 $2 $3
-    # echo "Finished AWQ"
-    # # AWQ part 2
-    # echo "Running AWQ Quantization"
-    # run_quantize $1 $2 $3 $4 $5
-    # echo "Finished AWQ Quantization"
-    # AWQ part 3
-    echo "Evaluating AWQ Quantization"
-    test_quantize $1 $2 $4 $5 $6 $7
-    echo "Evaluating AWQ Quantization"
+    # AWQ
+    echo "Running AWQ"
+    run_awq $1 $2 $3
+    echo "Finished AWQ"
+    # AWQ part 2
+    echo "Running AWQ Quantization"
+    run_quantize $1 $2 $3 $4 $5 $6 $7
+    echo "Finished AWQ Quantization"
 }
 
 # ======= Wanda (+ 30% kept) + AWQ =======
 
-wanda_dir="wanda_wmetric_awq"
-awq_dir="wanda_awq_wmetric_awq"
+# wanda_dir="wanda_wmetric_awq"
+# awq_dir="wanda_awq_wmetric_awq"
 
-for sparsity in 0.3; do
-    wanda_awq "meta-llama/Llama-2-7b-hf" \
-        $sparsity "unstructured" "out/$wanda_dir/wanda$sparsity" 4096 \
-        "out/perplexities/$wanda_dir/wanda${sparsity}eval4k.txt" 4 \
-        "out/$awq_dir/awq${sparsity}/awq_results" \
-        "out/$awq_dir/awq${sparsity}/quant_dump" "real" 2048 \
-        "out/perplexities/$awq_dir/awq${sparsity}eval2k.txt"
-done
+# for sparsity in 0.3; do
+#     wanda_awq "meta-llama/Llama-2-7b-hf" \
+#         $sparsity "unstructured" "out/$wanda_dir/wanda$sparsity" 4096 \
+#         "out/perplexities/$wanda_dir/wanda${sparsity}eval4k.txt" 4 \
+#         "out/$awq_dir/awq${sparsity}/awq_results" \
+#         "out/$awq_dir/awq${sparsity}/quant_dump" "real" 2048 \
+#         "out/perplexities/$awq_dir/awq${sparsity}eval2k.txt"
+# done
 
 # # ======= Wanda (+ 30% kept) + AWQ eval (2k, 4k) =======
 
@@ -114,22 +101,88 @@ done
 # wanda_dir="wanda_wmetric_awqandwanda"
 # awq_dir="wanda_awq_wmetric_awqandwanda"
 
-# for sparsity in 0.1 0.2; do
+# for sparsity in 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
 #     wanda_awq "meta-llama/Llama-2-7b-hf" \
 #         $sparsity "unstructured" "out/$wanda_dir/wanda$sparsity" 4096 \
 #         "out/perplexities/$wanda_dir/wanda${sparsity}eval4k.txt" 4 \
 #         "out/$awq_dir/awq${sparsity}/awq_results" \
-#         "out/$awq_dir/awq${sparsity}/quant_dump" "real" 2048 \
+#         "out/$awq_dir/awq${sparsity}/quant_dump" "fake" 2048 \
 #         "out/perplexities/$awq_dir/awq${sparsity}eval2k.txt"
 # done
 
 # # ======= Wanda (+ 30% kept) + AWQ eval (2k, 4k) =======
 
-# for sparsity in 0.1 0.2; do
+# for sparsity in 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
 #     wanda_awq "out/$wanda_dir/wanda$sparsity" \
 #         0 "unstructured" "out/$wanda_dir/wanda$sparsity" 2048 \
 #         "out/perplexities/$wanda_dir/wanda${sparsity}eval2k.txt" 4 \
 #         "out/$awq_dir/awq${sparsity}/awq_results" \
-#         "out/$awq_dir/awq${sparsity}/quant_dump" "real" 4096 \
+#         "out/$awq_dir/awq${sparsity}/quant_dump" "fake" 4096 \
 #         "out/perplexities/$awq_dir/awq${sparsity}eval4k.txt"
 # done
+
+# ======= Wanda (Different Layers Scaled) + AWQ =======
+
+# wanda_dir="wanda_wmetric_layered"
+# awq_dir="wanda_awq_wmetric_layered"
+
+# for sparsity in 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+#     wanda_awq "meta-llama/Llama-2-7b-hf" \
+#         $sparsity "unstructured" "out/$wanda_dir/wanda$sparsity" 4096 \
+#         "out/perplexities/$wanda_dir/wanda${sparsity}eval4k.txt" 4 \
+#         "out/$awq_dir/awq${sparsity}/awq_results" \
+#         "out/$awq_dir/awq${sparsity}/quant_dump" "fake" 2048 \
+#         "out/perplexities/$awq_dir/awq${sparsity}eval2k.txt"
+# done
+
+# ======= Wanda (Different layers scaled) + AWQ eval (2k, 4k) =======
+
+# for sparsity in 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+#     wanda_awq "out/$wanda_dir/wanda$sparsity" \
+#         0 "unstructured" "out/$wanda_dir/wanda$sparsity" 2048 \
+#         "out/perplexities/$wanda_dir/wanda${sparsity}eval2k.txt" 4 \
+#         "out/$awq_dir/awq${sparsity}/awq_results" \
+#         "out/$awq_dir/awq${sparsity}/quant_dump" "fake" 4096 \
+#         "out/perplexities/$awq_dir/awq${sparsity}eval4k.txt"
+# done
+
+# ======= Wanda (Different layers scaled) + AWQ Structured =======
+
+# wanda_awq "meta-llama/Llama-2-7b-hf" \
+#     0.5 "4:8" "out/$wanda_dir/wanda4_8" 4096 \
+#     "out/perplexities/$wanda_dir/wanda4_8eval4k.txt" 4 \
+#     "out/$awq_dir/awq4_8/awq_results" \
+#     "out/$awq_dir/awq4_8/quant_dump" "fake" 2048 \
+#     "out/perplexities/$awq_dir/awq4_8eval2k.txt"
+
+# wanda_awq "out/$wanda_dir/wanda4_8" \
+#     0 "unstructured" "out/$wanda_dir/wanda4_8" 2048 \
+#     "out/perplexities/$wanda_dir/wanda4_8eval2k.txt" 4 \
+#     "out/$awq_dir/awq4_8/awq_results" \
+#     "out/$awq_dir/awq4_8/quant_dump" "fake" 4096 \
+#     "out/perplexities/$awq_dir/awq4_8eval4k.txt"
+
+# ======= Wanda (Normalized Wanda + AWQ) + AWQ Structured =======
+
+wanda_dir="wanda_wmetric_normalized"
+awq_dir="wanda_awq_wmetric_normalized"
+
+for sparsity in 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+    wanda_awq "meta-llama/Llama-2-7b-hf" \
+        $sparsity "unstructured" "out/$wanda_dir/wanda$sparsity" 4096 \
+        "out/perplexities/$wanda_dir/wanda${sparsity}eval4k.txt" 4 \
+        "out/$awq_dir/awq${sparsity}/awq_results" \
+        "out/$awq_dir/awq${sparsity}/quant_dump" "fake" 2048 \
+        "out/perplexities/$awq_dir/awq${sparsity}eval2k.txt"
+done
+
+# ======= Wanda (+ 30% kept) + AWQ eval (2k, 4k) =======
+
+for sparsity in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+    wanda_awq "out/$wanda_dir/wanda$sparsity" \
+        0 "unstructured" "out/$wanda_dir/wanda$sparsity" 2048 \
+        "out/perplexities/$wanda_dir/wanda${sparsity}eval2k.txt" 4 \
+        "out/$awq_dir/awq${sparsity}/awq_results" \
+        "out/$awq_dir/awq${sparsity}/quant_dump" "fake" 4096 \
+        "out/perplexities/$awq_dir/awq${sparsity}eval4k.txt"
+done
