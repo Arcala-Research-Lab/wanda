7fc41231bbfe0e92f8087f97a34c704cfbf18b3f
Omar Yahia <oyahia@uci.edu>
2025-03-30 18:49:12 -0700

    baseline code for RL w/ wanda

---
+++ rl_compress.py
@@ -0,0 +1,21 @@
+import argparse
+from main import get_llm
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, help='LLaMA model')
+    parser.add_argument('--seed', type=int, default=0, help='Seed for sampling the calibration data.')
+    parser.add_argument('--nsamples', type=int, default=128, help='Number of calibration samples.')
+    parser.add_argument('--sparsity_ratio', type=float, default=0, help='Sparsity level')
+    parser.add_argument("--sparsity_type", type=str, choices=["unstructured", "4:8", "2:4"])
+    parser.add_argument("--eval_seqlen", type=int, default=2048)
+    parser.add_argument("--cache_dir", default="llm_weights", type=str )
+    parser.add_argument('--use_variant', action="store_true", help="whether to use the wanda variant described in the appendix")
+    parser.add_argument('--save', type=str, default=None, help='Path to save results.')
+    parser.add_argument('--save_model', type=str, default=None, help='Path to save the pruned model.')
+    args = parser.parse_args()
+
+    get_llm(args.model)
+
+if __name__ == '__main__':
+    main()
